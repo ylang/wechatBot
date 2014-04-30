b9d6f563f405bd173e4f6f9a0ec77652ed9015bd
ylang <ylang@andrew.cmu.edu>
2014-04-30 02:45:40 -0700

    play around

--- findMe/app.js
+++ findMe/app.js
@@ -35,6 +35,7 @@ if ('development' == app.get('env')) {
 //app.get('/', routes.index);
 //app.get('/users', user.list);
 app.use('/wechat', wechat('ylang', function (req, res, next) {
+  console.log("[WECHAT] get request " + req.weixin);
   // 微信输入信息都在req.weixin上
   var message = req.weixin;
   res.reply(message);
